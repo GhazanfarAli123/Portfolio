b12495aa81d851c96468cded73f2e04f110a5f35
Ghazanfar Ali <ghazanfar.alizahid@gmail.com>
2023-01-21 07:21:31 -0800

    @portfolio

--- ghazanfar-ali-portfolio/src/Components/header.js
+++ ghazanfar-ali-portfolio/src/Components/header.js
@@ -11,15 +11,15 @@ const Header = () => {
             <ul>
                 <img className="prof-image" src={images} alt="ghazanfar ali" />
                 <h1 className="name">Ghazanfar Ali</h1>
-                <li className="option-1 options">home</li>
-                <li className="options">about us</li>
-                <li className="options">contact us</li>
-                <li className="options">services</li>
+                <li className="option-1 options">Home</li>
+                <li className="options">About</li>
+                <li className="options">Contact</li>
+                <li className="options">Services</li>
                 <ul className="social-icons">
-                    <li className="icons icon-1 fa-brands fa-facebook-f"></li>
-                    <li className="icons">hallo</li>
-                    <li className="icons">hallo</li>
-                    <li className="icons">hallo</li>
+                    <li className="icons icon-1 fa fa-facebook" aria-hidden="true"></li>
+                    <li className="icons fa fa-github" aria-hidden="true"></li>
+                    <li className="icons fa fa-twitter" aria-hidden="true"></li>
+                    <li className="icons fa fa-linkedin" aria-hidden="true"></li>
                 </ul>
             </ul>
             </nav>
