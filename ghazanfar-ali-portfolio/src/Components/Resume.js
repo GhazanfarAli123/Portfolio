639144f44b0bb41898326331bd9ca3d8dd657a1c
Ghazanfar Ali <ghazanfar.alizahid@gmail.com>
2023-02-20 08:18:38 -0800

    portfolio changes

--- ghazanfar-ali-portfolio/src/Components/Resume.js
+++ ghazanfar-ali-portfolio/src/Components/Resume.js
@@ -4,8 +4,111 @@ export const Resume = () => {
     <div className="resume" id="resume">
       <div className="resume-back-text">SUMMARY</div>
       <div className="resume-front">
-      Resume
+        Resume
       </div>
+      <div className="ex-ed">
+        <div className="education">
+          My Education
+          <div className="first-education">
+            <div className="date">2021-Present</div>
+            <div className="degree">Computer Sciemce</div>
+            <div className="institution">Virtual University</div>
+            <p>Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.
+
+            </p>
+          </div>
+          <div className="first-education">
+            <div className="date-2">2018-2021</div>
+            <div className="degree">ACCP Prime Diploma</div>
+            <div className="institution">Aptech Computer Education</div>
+            <p>Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.
+
+            </p>
+          </div>
+          <div className="first-education">
+            <div className="date">2021-Present</div>
+            <div className="degree">Computer Sciemce</div>
+            <div className="institution">Virtual University</div>
+            <p>Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.
+
+            </p>
+          </div>
+        </div>
+        <div className="experience">My Experience
+          <div className="first-education">
+            <div className="date">2022-Present</div>
+            <div className="degree">Frontend Developer</div>
+            <div className="institution">Bramerz</div>
+            <p>Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.
+
+            </p>
+          </div>
+          <div className="first-education">
+            <div className="date">2022-Present</div>
+            <div className="degree">Frontend Developer</div>
+            <div className="institution">Bramerz</div>
+            <p>Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.
+
+            </p>
+          </div>
+          <div className="first-education">
+            <div className="date">2022-Present</div>
+            <div className="degree">Frontend Developer</div>
+            <div className="institution">Bramerz</div>
+            <p>Lisque persius interesset his et, in quot quidam persequeris vim, ad mea essent possim iriure.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="skills">
+        <div className="myskills">
+          My Skills
+          <div className="skill-name-perc">
+            HTML
+            <span>80%</span>
+            <div className="skill-perc">
+              <div className="skill-perc-2"></div>
+            </div>
+          </div>
+          <div className="skill-name-perc">
+            HTML
+            <span>80%</span>
+            <div className="skill-perc">
+              <div className="skill-perc-2"></div>
+            </div>
+          </div>
+          <div className="skill-name-perc">
+            HTML
+            <span>80%</span>
+            <div className="skill-perc">
+              <div className="skill-perc-2"></div>
+            </div>
+          </div>
+        </div>
+        <div className="skils-2">
+          <div className="skill-name-perc">
+            HTML
+            <span>80%</span>
+            <div className="skill-perc">
+              <div className="skill-perc-2"></div>
+            </div>
+          </div> <div className="skill-name-perc">
+            HTML
+            <span>80%</span>
+            <div className="skill-perc">
+              <div className="skill-perc-2"></div>
+            </div>
+          </div> <div className="skill-name-perc">
+            HTML
+            <span>80%</span>
+            <div className="skill-perc">
+              <div className="skill-perc-2"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   )
 }
